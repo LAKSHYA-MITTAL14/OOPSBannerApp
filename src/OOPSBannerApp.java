4c74e4354084c16755b15ae6739e65902e61a8c5
LAKSHYA-MITTAL14 <lakshyamittal14@gmail.com>
2026-03-10 21:09:54 +0530

    feature/UC5

--- src/OOPSBannerApp.java
+++ src/OOPSBannerApp.java
@@ -1,61 +1,17 @@
-public class Main {
-
-    public static String[] getAPattern() {
-        return new String[] {
-            "   *** ",
-            " **  **",
-            "**   **",
-            "**   **",
-            "**   **",
-            "**  ** ",
-            "  ***  "
-        };
-    }
-
-    public static String[] getBPattern() {
-        return new String[] {
-            "   *** ",
-            " **  **",
-            "**   **",
-            "**   **",
-            "**   **",
-            "**  ** ",
-            "  ***  "
-        };
-    }
-
-    public static String[] getCPattern() {
-        return new String[] {
-            "  ***** ",
-            "  **  **",
-            "  **  **",
-            "  ***** ",
-            "  **    ",
-            "  **    ",
-            "  **    "
-        };
-    }
-
-    public static String[] getDPattern() {
-        return new String[] {
-            "  ***** ",
-            " **     ",
-            " **     ",
-            "  ***** ",
-            "       **",
-            "  **   **",
-            "   ***** "
-        };
-    }
-
+public class OOPSBannerApp{
     public static void main(String[] args) {
-        String aPattern [] = getAPattern();
-        String cPattern[] = getCPattern();
-        String bPattern[] = getBPattern();
-        String dPattern[] = getDPattern();
-
-        for (int i = 0; i < aPattern.length; i++) {
-            System.out.println(aPattern[i] + " " + bPattern[i] + " " + cPattern[i] + " " + dPattern[i]);
-        }
+    String[] lines = {
+        String.join(" ","   ***   " , " " , "   ***   " , " " , " ******   " , " " , "   *****     "),
+        String.join(" "," **   ** " , " " , " **   ** " , " " , " **    ** " , " " , "  **    **   "),
+        String.join(" "," **   ** " , " " , " **   ** " , " " , " **    ** " , " " , " **          "),
+        String.join(" "," **   ** " , " " , " **   ** " , " " , " ******   " , " " , "   *****     "),
+        String.join(" "," **   ** " , " " , " **   ** " , " " , " **       " , " " , "         **  "),
+        String.join(" "," **   ** " , " " , " **   ** " , " " , " **       " , " " , " **     **   "),
+        String.join(" ","   ***   " , " " , "   ***   " , " " , " **       " , " " , "   *****     ")
+    };
+    for(String line : lines){
+        System.out.println(line);
+    }
+    
     }
 }
